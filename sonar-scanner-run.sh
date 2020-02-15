05744c309890f2e469f37b6f60acc2bdc721ab5e
Daniel Widerin <daniel@widerin.net>
2020-02-15 17:21:58 +0100

    rename deprecated to log_dropped
    Signed-off-by: Daniel Widerin <daniel@widerin.net>

--- sonar-scanner-run.sh
+++ sonar-scanner-run.sh
@@ -18,44 +18,44 @@ if [ ! -z ${SONAR_DEBUG+x} ]; then
   COMMAND="$COMMAND -X"
 fi
 
-deprecated() {
+log_dropped() {
     echo "[WARN] $1 has been ignored, please set '$2' in your 'sonar-project.properties' file."
 }
 
 if [ ! -z ${SONAR_PROJECT_KEY+x} ]; then
-    deprecated SONAR_PROJECT_KEY sonar.projectKey
+    log_dropped SONAR_PROJECT_KEY sonar.projectKey
 fi
 
 if [ ! -z ${SONAR_PROJECT_VERSION+x} ]; then
-    deprecated SONAR_PROJECT_VERSION sonar.projectVersion
+    log_dropped SONAR_PROJECT_VERSION sonar.projectVersion
 fi
 
 if [ ! -z ${SONAR_GITLAB_PROJECT_ID+x} ]; then
-    deprecated SONAR_GITLAB_PROJECT_ID
+    log_dropped SONAR_GITLAB_PROJECT_ID
 fi
 
 if [ ! -z ${SONAR_SOURCES+x} ]; then
-    deprecated SONAR_SOURCES sonar.sources
+    log_dropped SONAR_SOURCES sonar.sources
 fi
 
 if [ ! -z ${SONAR_PROFILE+x} ]; then
-    deprecated SONAR_PROFILE sonar.profile
+    log_dropped SONAR_PROFILE sonar.profile
 fi
 
 if [ ! -z ${SONAR_LANGUAGE+x} ]; then
-    deprecated SONAR_LANGUAGE sonar.language
+    log_dropped SONAR_LANGUAGE sonar.language
 fi
 
 if [ ! -z ${SONAR_ENCODING+x} ]; then
-    deprecated SONAR_ENCODING sonar.sourceEncoding
+    log_dropped SONAR_ENCODING sonar.sourceEncoding
 fi
 
 if [ ! -z ${SONAR_BRANCH+x} ]; then
-    deprecated SONAR_BRANCH sonar.branch.name
+    log_dropped SONAR_BRANCH sonar.branch.name
 fi
 
 if [ ! -z ${SONAR_ANALYSIS_MODE+x} ]; then
-    deprecated SONAR_BRANCH sonar.branch.name
+    log_dropped SONAR_BRANCH sonar.branch.name
 fi
 
 $COMMAND $@
